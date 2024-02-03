08156278ae5845a8143ff09775b829f43fe30237
dooshu <doosho@hotmail.com>
2024-02-03 19:39:31 +0800

    昭明文选

--- cn/files/superscript.js
+++ cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../628.txt'
+const filename = '../48.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
@@ -21,8 +21,8 @@ const filename = '../628.txt'
         reg = /(\d)(\d)?(\d)?/g
         reg = /〔(\d)(\d)?(\d)?〕/g
         reg = /\((\d)(\d)?(\d)?\)/g
-        reg = /\[(\d)(\d)?(\d)?\]/g
         reg = /\((\d)(\d)?(\d)?\)/g
+        reg = /\[(\d)(\d)?(\d)?\]/g
 
         result = data.replace(reg, ($1, $2, $3, $4, $5)=>{
           console.log($3)
