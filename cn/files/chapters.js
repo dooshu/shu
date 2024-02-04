aacf8dd847f29bb12f05116f804991e4132d0fd4
dooshu <doosho@hotmail.com>
2024-02-04 08:27:09 +0800

    诗话总龟 - (宋)阮阅

--- cn/files/chapters.js
+++ cn/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //const bookid = process.argv[2];
-const bookid = 674
+const bookid = 675
 let bookdata = fs.readFileSync('../'+bookid+'.txt', 'utf8')
 //bookdata = bookdata.replace(/\r\n/g,'\n')
 if(/\r/.test(bookdata)){
