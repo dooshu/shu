99bd48bffeb3a51dca4f197e642b7738ac5e62ab
dooshu <doosho@hotmail.com>
2024-02-10 19:18:59 +0800

    离骚

--- cn/files/superscript.js
+++ cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../685.txt'
+const filename = '../705.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
@@ -19,10 +19,10 @@ const filename = '../685.txt'
 
         var reg = /\[(\d)(\d)?(\d)?\]/g
         reg = /(\d)(\d)?(\d)?/g
-        reg = /〔(\d)(\d)?(\d)?〕/g
         reg = /\((\d)(\d)?(\d)?\)/g
         reg = /\((\d)(\d)?(\d)?\)/g
         reg = /\[(\d)(\d)?(\d)?\]/g
+        reg = /〔(\d)(\d)?(\d)?〕/g
 
         result = data.replace(reg, ($1, $2, $3, $4, $5)=>{
           console.log($3)
