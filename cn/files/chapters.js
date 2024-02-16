4237d5e8d2c42486610aa98f963a149cdbb6a8e2
dooshu <doosho@hotmail.com>
2024-02-16 19:50:39 +0800

    从华国锋下台到胡耀邦下台 - 胡绩伟

--- cn/files/chapters.js
+++ cn/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //const bookid = process.argv[2];
-const bookid = 723
+const bookid = 726
 let bookdata = fs.readFileSync('../'+bookid+'.txt', 'utf8')
 //bookdata = bookdata.replace(/\r\n/g,'\n')
 if(/\r/.test(bookdata)){
@@ -56,7 +56,7 @@ reg = /\n(上|徐爱录|陆澄录|后记|[一二三四五六七八九十〇]{1,3
 reg = /\n(第\d{1,5}章 .*|第[一二三四五六七八九十百零]{1,5}[则] .*)\n/g
 reg = /\n(卷.{1,3} .*)\n/g
 reg = /\n(.{2,3})\n/g
-reg = /\n(.{3,8}日.*)\n/g
+reg = /\n(.{1,60}[^。！：，日]\n)\n/g
 
 
 
