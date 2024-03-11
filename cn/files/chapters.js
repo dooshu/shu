b76015f74ac2106e84ddf208af9372b58c12c157
dooshu <doosho@hotmail.com>
2024-03-11 11:37:26 +0800

    珍珠之河 - VALLENNOX

--- cn/files/chapters.js
+++ cn/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //const bookid = process.argv[2];
-const bookid = 783
+const bookid = 784
 let bookdata = fs.readFileSync('../'+bookid+'.txt', 'utf8')
 //bookdata = bookdata.replace(/\r\n/g,'\n')
 if(/\r/.test(bookdata)){
@@ -55,7 +55,7 @@ reg = /\n(第[0-9]{1,5}卷|第[一二三四五六七八九十百零]{1,5}章　.
 reg = /\n(上|徐爱录|陆澄录|后记|[一二三四五六七八九十〇]{1,3}|[0-9]{1,3})\n/g
 reg = /\n(第\d{1,5}章 .*|第[一二三四五六七八九十百零]{1,5}[则] .*)\n/g
 reg = /\n([一二三四五六七八九十]{1,3}.*)\n/g
-reg = /\n(Part .*|\d{1,3}.)\n/g
+reg = /\n(第.{1,5}章：.*|哥哥)\n/g
 
 
 
