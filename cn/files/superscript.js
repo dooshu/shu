796d6670ececd28611207e421470c85671020253
dooshu <doosho@hotmail.com>
2024-04-13 10:28:02 +0800

    中山狼传

--- cn/files/superscript.js
+++ cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../56.txt'
+const filename = '../909.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
@@ -22,6 +22,7 @@ const filename = '../56.txt'
         reg = /〔(\d)(\d)?(\d)?〕/g
         reg = /\((\d)(\d)?(\d)?\)/g
         reg = /\[(\d)(\d)?(\d)?(\d)?(\d)?\]/g
+        reg = /（(\d)(\d)?(\d)?(\d)?(\d)?）/g
 
         result = data.replace(reg, ($1, $2, $3, $4, $5)=>{
           console.log($3)
