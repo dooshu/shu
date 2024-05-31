e185d39d45e00effb0ab75f22390f36e6066a5e6
dooshu <doosho@hotmail.com>
2024-05-31 20:12:47 +0800

    陶渊明集 - (东晋)陶潜

--- cn/files/superscript.js
+++ cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../223.txt'
+const filename = '../899.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
