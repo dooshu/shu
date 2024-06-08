051ebff6c9ba2e7a235db638e7d0a720b37b4d62
dooshu <doosho@hotmail.com>
2024-06-08 10:45:40 +0800

    论语 - (春秋)孔子

--- cn/files/superscript.js
+++ cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../1135.txt'
+const filename = '../33.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
