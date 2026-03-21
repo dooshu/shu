1a36cbf5a9e9e3fd91358d77bd1eff1ca5c7e792
dooshu <doosho@hotmail.com>
2026-03-21 19:36:46 +0800

    杜甫诗全集 钱注杜诗/钱牧斋先生笺注杜工部集 - (唐)杜甫、(清)钱谦益 秦观词全集 - (共)石海光 秦观词集 - (共)徐培均 淮海集 - (宋)秦观 七修類稿 - (明)郎锳 棠村词 - (清)梁清标 琅嬛记 - (元)伊世珍 道咸宦海见闻录 - (清)张集馨 甲申传信录 - (明清)钱甹只 民抄董宦事实 - (明)佚名 桓子新论 - (汉)桓谭/桓君山 庭闻录 - (清)刘健 四婵娟 - (明清)洪昇 金花女 - (明)吴守礼 晦庵先生朱文公文集、續集、别集 - (宋)朱熹 追问 - (共)丁捷 Islamic Exceptionalism: How the Struggle Over Islam Is Reshaping the World by Shadi Hamid Broken Country by Clare Leslie Hall

--- cn/files/superscript.js
+++ cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../1448.txt'
+const filename = '../36.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
